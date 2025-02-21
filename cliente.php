e252e57a0443913c68caf6f643211e358d861f1d
santiagogiraldo <santiagog5296@gmail.com>
2025-02-21 09:44:24 -0500

    Taller 2 (Parte 2): se agrego las variables usuario y clave, tambien hace un echo el cual muestra si coincide o no con los datos de la base de datos.

--- cliente.php
+++ cliente.php
@@ -9,13 +9,18 @@
     $nombre = "Usuario";
     $num1 = 10;
     $num2 = 5;
+    $usuario = "santiago";
+    $clave = "santiagito";
 
 
     echo $client->saludar($nombre . '!!!') . "</br>";
     echo "El Resultado de la suma es: " . $client->operacion($num1,$num2,'+') . "</br>";  
     echo "El Resultado de la resta es: " . $client->operacion($num1,$num2,'-') . "</br>";
     echo "El Resultado de la multiplicacion es: " . $client->operacion($num1,$num2,'*') . "</br>";
-    echo "El Resultado de la division es: " . $client->operacion($num1,$num2,'/') . "</br>";
+    echo "El Resultado de la division es: " . $client->operacion($num1,$num2,'/') . "</br>". "</br>";
+
+    echo "El producto es: ".$client->getProduct(). "</br>";
+    echo "Validando:"." ".$client->validarUsuario($usuario, $clave) . "<br/>";
 ?>
 
 
