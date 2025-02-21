83817401969c56c1db6037e2103407bfc64e45af
santiagogiraldo <santiagog5296@gmail.com>
2025-02-21 08:52:10 -0500

    Taller 2 (Parte1): En la funcion operacion se agrega una nueva variable llamada calcular la cual utiliza un switch para devolver los resultados dependiendo de la operacion correspondiente, de este manera se trabaja desde el mismo metodo.

--- server.php
+++ server.php
@@ -1,3 +1,39 @@
 <?php
-	
+    require('conexion.php');
+    
+    class serverSoap extends Conexion
+    {
+        public function saludar($name)
+        {
+            return 'Hola, '.$name.'!';
+        }
+    
+        public function operacion($num1, $num2, $calcular)
+        {
+           switch($calcular){
+            case '+':
+                return $num1 + $num2;
+            case '-':
+                return $num1 - $num2;
+            case '*':
+                return $num1 * $num2;
+            case '/':
+                return $num1 / $num2;        
+           }
+        }
+    }
+    
+    // Crear un nuevo servidor SOAP
+    $options = array('uri'=>'http://localhost/webservices/appwebservices/');
+    
+    // Instanciar el servidor SOAP
+    $server = new SoapServer(NULL, $options);
+    
+    // Configurar que clase manejara las solicitudes SOAP
+    $server->setClass('serverSoap');
+    
+    // Procesar las solicitudes SOAP
+    $server->handle();
+  
 ?>
+
