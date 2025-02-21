65d303e017112ac899abc1026a141089f9a9a48e
santiagogiraldo <santiagog5296@gmail.com>
2025-02-21 09:08:51 -0500

    Taller 2 (Parte1): Se modifica un poco para mostrar los resultados de cada operacion

--- cliente.php
+++ cliente.php
@@ -1,3 +1,22 @@
 <?php
-	
+    $options = array(
+        'location' => 'http://localhost/webservices/appwebservices/server.php',
+        'uri'      => 'http://localhost/webservices/appwebservices'
+    );
+    
+    $client = new SoapClient(NULL, $options);
+    
+    $nombre = "Usuario";
+    $num1 = 10;
+    $num2 = 5;
+
+
+    echo $client->saludar($nombre . '!!!') . "</br>";
+    echo "El Resultado de la suma es: " . $client->operacion($num1,$num2,'+') . "</br>";  
+    echo "El Resultado de la resta es: " . $client->operacion($num1,$num2,'-') . "</br>";
+    echo "El Resultado de la multiplicacion es: " . $client->operacion($num1,$num2,'*') . "</br>";
+    echo "El Resultado de la division es: " . $client->operacion($num1,$num2,'/') . "</br>";
 ?>
+
+
+
